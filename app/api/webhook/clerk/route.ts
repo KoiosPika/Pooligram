3f62cff32d8c7b80339c590147836ac43f9ff933
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-01-17 18:14:58 -0800

    setup clerk webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser } from '@/lib/database/actions/user.actions'
+import { createUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
  

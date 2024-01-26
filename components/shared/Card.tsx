a0b3a8984bc4e05aa828d7d19bd331eb6ef85651
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-01-25 18:02:16 -0800

    card ui

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 
 const Card = ({ poll }: { poll: IPoll }) => {
     return (
-        <div className='flex flex-col h-[350px] w-[350px] rounded-xl mx-1'>
+        <div className='flex flex-col min-h-[350px] bg-yellow-800 w-[350px] rounded-xl mx-1'>
             <Link href={`/poll/${poll._id}`} className='flex flex-center h-[250px] bg-gray-50 text-grey-500 rounded-t-lg'>
                 <Image src={poll.imageUrl} alt='hero' height={400} width={400} className='w-[350px] h-[350px] overflow-hidden'/>
             </Link>
-            <div className='h-[155px] w-full p-2 flex flex-col justify-between bg-blue-800 border-white border-t-2 rounded-b-lg'>
+            <div className='flex-1 w-full p-2 flex flex-col justify-between bg-blue-800 border-white border-t-2 rounded-b-lg'>
                 <p className='font-semibold truncate-2-lines text-white text-[14px]'>{poll.title}</p>
                 <div className='flex flex-row items-center'>
                     <Image src={poll.creator.photo || '/assets/images/user.png'} alt='user' className='w-[35px] h-[35px] rounded-full border-2 border-white' height={100} width={100} />

d0358e0a4fdb15e822333a234ae080e084ce0f5a
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-01-25 17:58:26 -0800

    card ui

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -11,11 +11,11 @@ const Card = ({ poll }: { poll: IPoll }) => {
             </Link>
             <div className='h-[155px] w-full p-2 flex flex-col justify-between bg-blue-800 border-white border-t-2 rounded-b-lg'>
                 <p className='font-semibold truncate-2-lines text-white text-[14px]'>{poll.title}</p>
-                <Link href={`/poll/${poll._id}`} className='flex flex-row items-center bg-yellow-500'>
+                <div className='flex flex-row items-center'>
                     <Image src={poll.creator.photo || '/assets/images/user.png'} alt='user' className='w-[35px] h-[35px] rounded-full border-2 border-white' height={100} width={100} />
                     <p className='ml-2 font-semibold text-white'>{poll.creator.username}</p>
                     {poll.creator.verified && <Image className='ml-1' src={'/assets/icons/verified.svg'} alt='verified' height={18} width={18} />}
-                </Link>
+                </div>
             </div>
         </div>
     )
